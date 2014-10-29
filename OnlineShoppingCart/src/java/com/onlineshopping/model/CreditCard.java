a032e420882b2eb62cf79b702e727af4d12bc682
pmkrish6 <pmkrish6@172.17.0.38>
2014-10-29 13:21:52 -0500

    created java all classes

---
+++ OnlineShoppingCart/src/java/com/onlineshopping/model/CreditCard.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.onlineshopping.model;
+
+/**
+ *
+ * @author pmkrish6
+ */
+public class CreditCard {
+    private String state;
+    private int zipcode;
+    private String country;
+    private String cart_Id;
+    
+}
---
+++ OnlineShoppingCart/src/java/com/onlineshopping/model/User.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.onlineshopping.model;
+
+/**
+ *
+ * @author pmkrish6
+ */
+public class User {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String adress;
+}
