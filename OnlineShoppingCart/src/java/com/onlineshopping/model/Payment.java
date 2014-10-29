33cc646782cc0cc820fed2cf76aa2126341d2637
pmkrish6 <pmkrish6@172.17.0.38>
2014-10-28 22:30:38 -0500

    created java packages

---
+++ OnlineShoppingCart/src/java/com/onlineshopping/model/Payment.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.onlineshopping.model;
+
+/**
+ *
+ * @author pmkrish6
+ */
+public class Payment {
+    private String firstName;
+    private String lastName;
+    private String cardType;
+    private String cardNumber;
+    
+}
---
+++ OnlineShoppingCart/src/java/com/onlineshopping/model/Product.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.onlineshopping.model;
+
+/**
+ *
+ * @author pmkrish6
+ */
+public class Product {
+    private String name;
+    private double price;
+    private String ShippmentType;
+    private String approval;
+    
+}
